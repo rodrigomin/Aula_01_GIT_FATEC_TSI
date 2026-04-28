d08c4a96eba9fc1609318968e56de1d6426a8109
rodrigomin <rodrigo.minotic@gmail.com>
2026-04-27 22:49:58 -0300

    FIX: arrumando a centralização

--- aula05/ultimododia.py
+++ aula05/ultimododia.py
@@ -1,19 +1,19 @@
-max_line = 35
+max_line = 100
 
 print(f'=' * max_line)
-nMin = int(input("Digite o valor minimo: "))
-nMax = int(input("Digite o valor maximo: "))
+nMin = int(input(f"{" "*int((max_line/2)-11)}Digite o valor minimo: ")) #22/2 = 11
+nMax = int(input(f"{" "*int((max_line/2)-11)}Digite o valor maximo: ")) #22/2 = 11
 print(f'=' * max_line)
 
 
 for i in range(nMin, nMax + 1):
     if i % 2 == 0:
         print(f'=' * max_line)
-        print(f"{" "*int((max_line/2)-7)}Número {i} é par") # 14
+        print(f"{" "*int((max_line/2)-7)}Número {i} é par") # 14/2 = 7
         print(f'=' * max_line)
     else:
         print(f'=' * max_line)
-        print(f"{" "*int((max_line/2)-8)}Número {i} é impar") # 16
+        print(f"{" "*int((max_line/2)-8)}Número {i} é impar") # 16/2 = 8
         print(f'=' * max_line)
         
         
