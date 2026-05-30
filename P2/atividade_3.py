9f9758f025e51d555393d39756cb8388f3b01899
rodrigomin <rodrigo.minotic@gmail.com>
2026-05-30 09:41:54 -0300

    UPD: Atividade 3 Completa

--- P2/atividade_3.py
+++ P2/atividade_3.py
@@ -1,17 +1,17 @@
-soma_impares = 0
-cont_impares = 0
-numero = int(input("Digite um número (0 para sair): "))
+def errado():
+    soma_impares = 0
+    cont_impares = 0
+    numero = int(input("Digite um número (0 para sair): "))
 
-while numero != 0:
-    if numero % 2 != 0:
-        soma_impares +=numero
-        cont_impares +=1
-        print(soma_impares, cont_impares)
+    while numero != 0:
+        if numero % 2 != 0:
+            soma_impares = soma_impares + numero
+            cont_impares = cont_impares + 1
+        # fim do if
 
-# fim do if
     media = soma_impares / cont_impares
-    print("A média dos ímpares é: ", media)
-    
+    print("A média dos ímpares é: " + media)
+
     
 """
         O problema é: caso o primeiro número inserido não for impar, ele não conta nada, posteriormente tenta dividir
@@ -19,3 +19,37 @@ while numero != 0:
     while, o input só é chamado uma vez, e é fora do loop, oq causa um looping infinito, já que numero nunca poderá ser
     diferente uma vez definido.
 """
+
+
+def certo():
+    soma_impares = 0
+    cont_impares = 0
+
+    numero = 1
+    while numero != 0:
+        numero = int(input("Digite um número (0 para sair): "))
+
+        if numero % 2 != 0:
+            soma_impares += numero
+            cont_impares += 1
+            print(soma_impares, cont_impares)
+        else:
+            print("Esse número não é ímpar")
+
+    """
+        try:
+            media = soma_impares / cont_impares
+            print("A média dos ímpares é: ", media)
+        except(ZeroDivisionError):
+            print("ERRO: Não foi colocado números ímpares")
+    """
+
+    # OUUUUUU
+
+    if (soma_impares > 0):
+        media = soma_impares / cont_impares
+        print("A média dos ímpares é: ", media)
+    else:
+        print("ERRO: Não foi colocado números ímpares, portanto a média é ", 0)
+
+certo()
